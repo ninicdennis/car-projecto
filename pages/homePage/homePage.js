3515828a7b7dc9f71f89df6705fb163bbb8f102e
ninicdennis <pupperbot@protonmail.com>
2021-05-28 18:32:23 -0600

    Styling changes.

--- pages/homePage/homePage.js
+++ pages/homePage/homePage.js
@@ -53,23 +53,23 @@ export const HomePage = () => {
 					backgroundColor: '#b4b4b4',
 				}}
 			>
-				<Grid container wrap spacing='3' justify='center' alignItems='center'>
-					<Grid item xs='6'>
+				<Grid container spacing={3} justify='center' alignItems='center'>
+					<Grid item xs={6}>
 						<Typography style={{ textAlign: 'center' }}>Upload your own Header and Profile Picture</Typography>
 					</Grid>
-					<Grid item xs='6'>
+					<Grid item xs={6}>
 						<Image src='/media/placeholder-bg.png' width={200} height={200} />
 					</Grid>
-					<Grid item xs='6'>
+					<Grid item xs={6}>
 						<Typography style={{ textAlign: 'center' }}>Update your Vehicle Modification Page </Typography>
 					</Grid>
-					<Grid item xs='6'>
+					<Grid item xs={6}>
 						<Image src='/media/placeholder-bg.png' width={200} height={200} />
 					</Grid>
-					<Grid item xs='6'>
+					<Grid item xs={6}>
 						<Typography style={{ textAlign: 'center' }}>Follow some of your favorite project cars!</Typography>
 					</Grid>
-					<Grid item xs='6'>
+					<Grid item xs={6}>
 						<Image src='/media/placeholder-bg.png' width={200} height={200} />
 					</Grid>
 				</Grid>
